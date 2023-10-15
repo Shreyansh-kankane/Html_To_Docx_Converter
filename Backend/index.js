f20a5fa0c94e22f99127869fbfc97dc4b08ade92
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2023-10-15 22:46:40 +0530

    server

--- Backend/index.js
+++ Backend/index.js
@@ -20,17 +20,13 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + "-" + count);
     },
 });
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/public/html/index.html");
-// });
-
 
 const upload = multer({ storage: storage });
 
 app.post("/getDocx", upload.single("file"), (req, res) => {
-    console.log(req.file);
+    // console.log(req.file);
     count++;
-    console.log(count);
+    // console.log(count);
     const countTemp = count;
     if (!req.file) {
         return res.status(400).json({ message: "No files were uploaded." });
@@ -40,24 +36,21 @@ app.post("/getDocx", upload.single("file"), (req, res) => {
     console.log(req.file.originalname)
     fs.readFile(filePath, "utf8", (err, data) => {
         if (err) {
-            return res
-                .status(500)
-                .json({ error: "Error reading the uploaded file." });
+            return res.status(500).json({ error: "Error reading the uploaded file." });
         }
 
         const fileContent = data;
         var client = new grabzit(
-            "MjhkOGE2ZTk2YWMxNDg4ZWEzMWFmMjM3YWI0YjRiMzM=",
-            "Pxg/ED8/PwU/Kj8/Pz8/PzA/Pz8/JT9uOD8sBmk/ID8="
+            "YzU4YTljY2ZkOGQzNDlhMGJmODRiZjExNDA1YTU4MDA=",
+            "Lj8zPyo/Pyk/Pz8/Mz9tVQgEP1g/Pz8/FCY/P3YpP24="
         );
         client.html_to_docx(fileContent);
         client.save_to(`uploads/result${countTemp}.docx`, function (error, id) {
             if (error != null) {
-                console.log(err);
-                res
-                    .status(500)
-                    .json({ error: "Error reading the uploaded file." });
+                console.log(error);
+                return res.status(500).json({ error: "Error reading the uploaded file." });
             }
+            console.log("File saved at :" + id)
             res.sendFile(__dirname + `/uploads/result${countTemp}.docx`);
         });
     });
